e7213f40f3619ad3d1aef266cad0efa9ef8a8bfd
dudouro <150480310+dudouro@users.noreply.github.com>
2024-10-22 15:37:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ from financeira import get_financials_in_portuguese, analisar_fleuriet_df, anali
 # Interface Streamlit
 st.title("Análise Financeira de Ações")
 
-ticker = st.text_input("Digite o ticker da ação (ex: AAPL, MSFT):")+".SA"
+# Input do usuário para o ticker
+ticker = st.text_input("Digite o ticker da ação (ex: AMER3, BHIA3, MGLU3):")
 
-if ticker:
-    
-    balance_sheet = get_financials_in_portuguese(ticker)
+# Adicionar botão para realizar a pesquisa
+if st.button("Pesquisar"):
+    ticker = ticker + ".SA"  # Adicionar sufixo para ações brasileiras
 
-    # Análise utilizando as funções fornecidas
     try:
+        # Obter os dados financeiros
+        balance_sheet = get_financials_in_portuguese(ticker)
+
+        # Análise utilizando as funções fornecidas
         fleuriet_df = analisar_fleuriet_df(balance_sheet)
         dupont_df = analisar_dupont_df(balance_sheet)
         zscore_df = calcular_zscore_df(balance_sheet)
 
+        # Exibir os resultados das análises
         st.subheader("Análise Fleuriet")
         st.dataframe(fleuriet_df)
 
@@ -25,6 +30,6 @@ if ticker:
 
         st.subheader("Cálculo do Z-Score")
         st.dataframe(zscore_df)
-        
+
     except Exception as e:
         st.error(f"Ocorreu um erro ao realizar as análises: {e}")
